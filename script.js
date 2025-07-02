ebdfcaee7d8690170f2e00a167e8160ca28aa382
TeuzinM <mateus.moreira26@escola.pr.gov.br>
2025-07-02 19:39:52 +0000

    FINAL

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    const texto = document.querySelector('#entrada-de-texto').value;
+    const texto = document.querySelector('#entrada').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
     const palavrasChave = processaTexto(texto);
 
