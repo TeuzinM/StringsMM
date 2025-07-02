fdf4dbd9df849f75e1747ad97e1246ed9770ba88
TeuzinM <mateus.moreira26@escola.pr.gov.br>
2025-07-02 15:38:59 +0000

    java

---
+++ script.js
@@ -0,0 +1,6 @@
+const botaoMostraPalavras = document.querySelector('#boatao-PalvraChaveM');
+
+botaoMostraPalavras.addEventListener('click, mostraPalavrasChave');
+function mostraPalavrasChave (){
+    alert('fui clicado');
+}
